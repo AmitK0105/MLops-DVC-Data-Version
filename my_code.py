0569f3f3d7f1ef5b97bc2659f0455afa5afb810f
Amit Kumar <amitk.pr15@gmail.com>
2025-05-05 17:32:29 +0530

    First commint before DVC

---
+++ my_code.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import os
+
+#create a sample data frame
+
+data= {"Name":["Alice", "Charlie", "Bob"],
+       "Age": [25, 30, 35], 
+       "city":["New York", "Los Angles", "Chichago"]}
+       
+df= pd.DataFrame(data)
+
+#Ensure that data directory exist at root level
+
+data_dir= "Data"
+os.makedirs(data_dir,exist_ok=True)
+
+#Define the file path
+
+file_path= os.path.join(data_dir,"sample_data.csv")
+
+#save the data frame to csv
+
+df.to_csv(file_path,index=False)
+print("csv file saved to ", file_path)
---
+++ projectflow.md
@@ -0,0 +1,4 @@
+projectflow.txt
+1. create git repo and clone it into local
+create my_code.py and add code to it (it will save a csv file to a new "data" folder)
+do a git add commit push before initializing dvc
